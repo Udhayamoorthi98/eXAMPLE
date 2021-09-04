3ee92d4ddc94718bf550f52799657cdea97858c3
udhayamoorthi04 <udhayamoorthi60@gmail.com>
2021-09-04 16:40:50 +0530

    files

---
+++ search/typedefs_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['boxstruct',['BoxStruct',['../main1_8c.html#a718548848862b3d5da8f7c7cea677de1',1,'main1.c']]]
+];
---
+++ search/variables_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['b',['b',['../structBoxStruct__struct.html#aee44748dc579c5d794853ee8b133d197',1,'BoxStruct_struct']]],
+  ['boxenum',['BoxEnum',['../main1_8c.html#a00ffa5e9e639ba6c8155e780daec7474',1,'main1.c']]]
+];
